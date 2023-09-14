cf731858a659c41fbe9a7afb66a6e1a2113a21aa
raimonizard <97727989+raimonizard@users.noreply.github.com>
2023-09-14 18:38:56 +0200

    chore: stable version

--- main.ts
+++ main.ts
@@ -3,9 +3,15 @@ namespace SpriteKind {
 }
 
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    catcher.startEffect(effects.confetti, 500)
-    sprite.sayText(":)")
-    sprites.destroy(sprite, effects.disintegrate, 200)
+    catcher.startEffect(effects.confetti, 250)
+    sprite.sayText(":)", 200)
+    sprites.destroy(sprite, effects.disintegrate, 50)
+    music.play(music.melodyPlayable(music.magicWand), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function on_on_overlap_enemy(sprite: Sprite, otherSprite: Sprite) {
+    catcher.startEffect(effects.fire, 250)
+    sprites.destroy(sprite, effects.halo, 50)
+    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
 })
 controller.right.onEvent(ControllerButtonEvent.Repeated, function on_right_repeated() {
     catcher.x += 10
@@ -34,32 +40,54 @@ function throwTweet() {
     twitter_logo.setVelocity(randint(-4, 4), randint(10, 60))
 }
 
+function throwX() {
+    
+    x_logo.setStayInScreen(true)
+    x_logo.setBounceOnWall(true)
+    x_logo.setPosition(randint(5, 155), 0)
+    x_logo.setVelocity(randint(-4, 4), randint(10, 60))
+}
+
 sprites.onDestroyed(SpriteKind.Projectile, function on_on_destroyed(sprite2: Sprite) {
     info.setScore(info.score() + 100)
 })
 controller.left.onEvent(ControllerButtonEvent.Repeated, function on_left_repeated() {
     catcher.x += -10
 })
-let twitter_logo : Sprite = null
-let catcher : Sprite = null
-info.setScore(0)
-scene.setBackgroundImage(assets.image`
-    ElonMidSize
-`)
-game.setDialogTextColor(8)
-game.showLongText("Catch all the tweets before they fall!", DialogLayout.Top)
-catcher = sprites.create(assets.image`
-    catcher
-`, SpriteKind.Player)
-catcher.setPosition(80, 110)
-catcher.setStayInScreen(true)
-let index = 0
-while (index <= randint(15, 35)) {
-    throwTweet()
-    pause(randint(400, 1500))
-    index += 1
+function start() {
+    music.setVolume(50)
+    info.setScore(0)
+    scene.setBackgroundImage(assets.image`
+        ElonMidSize
+    `)
+    game.setDialogTextColor(8)
+    game.showLongText("Catch all the tweets before they fall!", DialogLayout.Top)
+    catcher.setPosition(80, 110)
+    catcher.setStayInScreen(true)
+    let index = 0
+    while (index <= randint(15, 45)) {
+        if (randint(0, 1) == 0) {
+            throwTweet()
+        } else {
+            throwX()
+        }
+        
+        pause(randint(400, 1500))
+        index += 1
+    }
+    pause(2000)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
 }
-pause(2000)
-game.setGameOverScoringType(game.ScoringType.HighScore)
-game.gameOver(true)
-game.setGameOverEffect(true, effects.confetti)
+
+let twitter_logo : Sprite = null
+let x_logo : Sprite = null
+x_logo = sprites.create(assets.image`
+        xLogo
+    `, SpriteKind.Enemy)
+let catcher : Sprite = null
+catcher = sprites.create(assets.image`
+        catcher
+    `, SpriteKind.Player)
+start()
